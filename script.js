8e0cdbe0de8d3e300a71bac23613bfa040cb8c34
Talha-Rasool <talharasool450@gmail.com>
2024-07-15 11:26:30 +0200

    `feat`:Add loan feature .

--- script.js
+++ script.js
@@ -172,19 +172,33 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+  if (
+    amount >= 0 &&
+    currentAccount.movements.some(mov => mov >= amount * 0.1)
+  ) {
+    currentAccount.movements.push(amount);
+
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
   if (
     inputCloseUsername.value === currentAccount.username &&
     Number(inputClosePin.value === currentAccount.pin)
   ) {
-    const index = accounts.findIndex( acc => acc.username === currentAccount.username);
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
 
-    
     accounts.splice(index, 1);
-    console.log(index)
     containerApp.style.opacity = 0;
-    
   }
   inputCloseUsername.value = inputClosePin.value = '';
 });
